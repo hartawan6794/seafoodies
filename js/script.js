9255e9cf96d5217bc91db8716dfc207a1fd502ab
juli1994 <julihartawan06@gmail.com>
2023-05-30 09:56:59 +0700

    perbaikan

--- js/script.js
+++ js/script.js
@@ -28,6 +28,21 @@ const humberger = document.querySelector('#hamburger-menu');
 const searchbtn = document.querySelector('#search-btn')
 const shoppingBtn = document.querySelector('#shopping-cart-btn');
 
+// modal box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButton = document.querySelector('.item-detail-button');
+const closeModal = document.querySelector('.close-icon');
+
+itemDetailButton.onclick = (e) => {
+    itemDetailModal.style.display = 'flex'
+    e.preventDefault();
+}
+
+closeModal.onclick = (e) => {
+    itemDetailModal.style.display = 'none'
+    e.preventDefault();
+}
+
 document.addEventListener('click', function(e){
     if(!humberger.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
@@ -40,7 +55,8 @@ document.addEventListener('click', function(e){
     if(!shoppingBtn.contains(e.target) && !shoppingCart.contains(e.target)){
         shoppingCart.classList.remove('active');
     }
-    
 
+    if(!itemDetailButton.contains(e.target) && !itemDetailModal.contains(e.target)){
+        // itemDetailModal.style.display = 'none'
+    }
 })
-
